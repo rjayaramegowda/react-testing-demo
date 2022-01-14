38bd15a045cdcbf37c4444259f405206b74d95d5
Ravichandran Jayaramegowda <50406692+rjayaramegowda@users.noreply.github.com>
2022-01-14 14:09:14 +0530

    cleanup

--- src/__tests__/Counter.test.js
+++ src/__tests__/Counter.test.js
@@ -2,17 +2,7 @@ import { fireEvent, render, screen } from "@testing-library/react";
 import Counter from "../Counter";
 
 test("<Counter />", () => {
-  const { debug } = render(<Counter />);
-  debug();
+  render(<Counter />);
   const btnIncrement = screen.getByText(/Increment/);
   fireEvent.click(btnIncrement);
-  fireEvent.click(btnIncrement);
-  debug();
-
-  const counterElement = screen.getByText(/2/);
-  expect(counterElement).toBeInTheDocument();
-
-  const btnDecrement = screen.getByText(/Increment/);
-  fireEvent.click(btnDecrement);
-  debug();
 });
